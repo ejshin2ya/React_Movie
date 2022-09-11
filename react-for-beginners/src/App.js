b99f2dc2584770ed1e8eae62135d1193aacfb611
신은정 <ejshin0213@naver.com>
2022-09-12 00:05:54 +0900

    7-6. Parameters

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/movie" element={<Detail />} />
+        <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,4 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 function Detail() {
-  return <h1>Detail</h1>;
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setLoading(false);
+    setMovie(json.data.movie);
+  };
+  console.log(movie);
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : (
+        <div>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <h2>{movie.title}</h2>
+          <p>개봉일:{movie.date_uploaded}</p>
+          <p>평점:{movie.rating}</p>
+          <p>상영시간:{movie.runtime}</p>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
